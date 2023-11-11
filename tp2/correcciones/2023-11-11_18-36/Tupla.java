ee8a8fa8a2c17ccaaaf10dc1413c82c2b9c9ba32
Algobot <algo2-doc@dc.uba.ar>
2023-11-11 18:36:31 -0300

    Hola,
    Hubo un error al tratar de compilar tu TP. Se encuentra adjunta la salida de error (stderr) generada al intentar compilar.
    
    Saludos cordiales,
    Algo2 Bot

---
+++ tp2/correcciones/2023-11-11_18-36/Tupla.java
@@ -0,0 +1,39 @@
+package aed;
+
+public class Tupla<A, B> {
+    private A primerElemento;
+    private B segundoElemento;
+
+    public Tupla(A primerElemento, B segundoElemento) {
+        this.primerElemento = primerElemento;
+        this.segundoElemento = segundoElemento;
+    }
+
+    public A getPrimerElemento() {
+        return primerElemento;
+    }
+
+    public B getSegundoElemento() {
+        return segundoElemento;
+    }
+
+    public void setPrimerElemento(A primerElemento) {
+        this.primerElemento = primerElemento;
+    }
+
+    public void setSegundoElemento(B segundoElemento) {
+        this.segundoElemento = segundoElemento;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + primerElemento + ", " + segundoElemento + ")";
+    }
+
+    public static void main(String[] args) {
+        Tupla<String, Integer> tupla = new Tupla<>("Hola", 42);
+        System.out.println("Primer Elemento: " + tupla.getPrimerElemento());
+        System.out.println("Segundo Elemento: " + tupla.getSegundoElemento());
+        System.out.println("Tupla completa: " + tupla);
+    }
+}
